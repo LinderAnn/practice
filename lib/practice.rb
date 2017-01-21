6b75d4841e5cab208ad5dd1a79228cc2416be5fe
Ann <linder.ann@gmail.com>
2017-01-21 16:51:57 -0500

    Add ERB view

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,5 +1,8 @@
-require "sinatra"
+load_dir = File.expand_path(File.dirname(__FILE__))
+$:.unshift(load_dir) unless $:.include?(load_dir)
 
+require "sinatra"
+require 'practice/version'
 get "/" do
- "Hello, World."
+    erb :index
 end
